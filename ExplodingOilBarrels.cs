0d082f7adcd5e8c7119c1c74c7f685aeee3bb4cf
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2019-07-10 08:19:05 +0100

    Create ExplodingOilBarrels.cs

---
+++ ExplodingOilBarrels.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using Oxide.Core.Plugins;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic; 
+
+namespace Oxide.Plugins
+{
+    [Info("Exploding oil barrel", "Bazz3l", "1.0.0")]
+    public class ExplodingOilBarrels : RustPlugin
+    {
+        string BarrelEffect = "assets/bundled/prefabs/fx/explosions/explosion_03.prefab";
+        string ShakeEffect  = "assets/prefabs/weapons/thompson/effects/attack_shake.prefab";
+
+        #region Oxide
+        void OnEntityDeath(BaseCombatEntity entity, HitInfo info)
+        {
+            if (entity == null || info == null)
+            {
+                return;
+            }
+
+            string damageType = Enum.GetName(typeof(Rust.DamageType), info.damageTypes.GetMajorityDamageType());
+            if (damageType != "Bullet")
+            {
+                return;
+            }
+
+            if (entity.ShortPrefabName == "oil_barrel")
+            {
+                Effect.server.Run(BarrelEffect, entity.transform.position, Vector3.zero, null, false);
+
+                List<BasePlayer> NearPlayers = new List<BasePlayer>();
+                Vis.Entities<BasePlayer>(entity.transform.position, 15, NearPlayers);
+
+                foreach(var player in NearPlayers)
+                {
+                    if (player != null && player.IsConnected)
+                    {
+                        for (int i = 0; i < 10; i++)
+                            Effect.server.Run(ShakeEffect, player.transform.position);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
